00c7ecb6fd73f4f80b0b246e3ee1e54e40a09438
Sebvi26 <sebastianvi26@gmail.com>
2023-09-25 13:25:00 +1100

    lectura de csv comentada

--- csvproj/read_csv.py
+++ csvproj/read_csv.py
@@ -1,13 +1,13 @@
 import csv
 
-def read_csv(path):
-  with open(path, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    header = next(reader)
-    data = []
-    for row in reader:
-      iterable = zip(header, row)
-      country_dict = {key: value for key, value in iterable}
+def read_csv(path):  #el path es la entrada, y debemos decir donde esta ubicado el archivo csv que vamos a leer
+  with open(path, 'r') as csvfile: #la r es para darle permiso solo de lectura, open es una instruccion que se da para abrir el archivo
+    reader = csv.reader(csvfile, delimiter=',')#csv.reader viene de el paquete csv, se tiene que dar el nombre del archivo y un delimitador que depende del csv
+    header = next(reader)#esto obtiene la primera fila de forma manual, el next se usa en iterables y da el siguiente valor, en este caso el primero
+    data = []#se crea una lista de diccionarios
+    for row in reader:#lee cada una de las filas, esta cada una en forma de lista
+      iterable = zip(header, row)#con zip se unen dos listas en este caso header con row en tuplas
+      country_dict = {key: value for key, value in iterable}#esto es un dictionary comprehension para convertir las tuplas en un diccionario
       data.append(country_dict)
     return data
 
