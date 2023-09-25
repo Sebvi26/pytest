d477c2e0e098d7a7d667fd689dbd1cb6ba44bd07
Sebvi26 <sebastianvi26@gmail.com>
2023-09-25 12:48:12 +1100

    lectura de csv subida

---
+++ prueba.py
@@ -0,0 +1,25 @@
+def sum_with_range(min, max):
+  print(min, max)
+  sum = 0
+  for x in range(min, max):
+    sum += x
+  return sum
+
+result = sum_with_range(1, 10)
+print(result)
+result_2 = sum_with_range(result, result + 10)
+print(result_2)
+
+def find_volume(length=1, width=1, depth=1):
+  return length * width * depth, width, 'hola'
+
+result, width, text = find_volume(width=10)
+
+print(result)
+print(width)
+print(text)
+
+full_name = lambda name, last_name: f'Full name is {name.title()} {last_name.title()}'
+
+text = full_name('nicolas', 'perez casas')
+print(text)
